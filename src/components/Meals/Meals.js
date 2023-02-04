9d43bc422f6ecc6b58ef0bfd41e594c5f529bd77
Neetu Seharawat <116377226+NeetuSeharawat@users.noreply.github.com>
2023-02-04 23:00:18 +0530

    updated

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 
 import MealsSummary from "./MealsSummary";
-//import AvailableMeals from './AvailableMeals';
+import AvailableMeals from './AvailableMeals';
 
 const Meals = () =>{
 
@@ -9,7 +9,7 @@ const Meals = () =>{
         <Fragment>
             <MealsSummary />
 
-         {/* <AvailableMeals /> */}
+          <AvailableMeals />
         </Fragment>
     )
 };
